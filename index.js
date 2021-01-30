e76df01445fb76c34b6637fff00c700400ba9b61
aeonincode <khashayar.sasannia@hotmail.com>
2021-01-30 21:09:05 +0100

    Testing OAuth

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ passport.use(
   )
 );
 
+// Add single route handler
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
