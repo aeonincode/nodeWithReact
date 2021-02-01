2f222b640b8beeb08fd44d0edabf11e3a6875178
aeonincode <khashayar.sasannia@hotmail.com>
2021-02-01 21:36:22 +0100

    MongoDB Atlas Setup and Configuration

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 
 require('./routes/authRoutes')(app);
 
+// Your root index.js connect function should still look like this:
+// mongoose.connect(keys.mongoURI);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
