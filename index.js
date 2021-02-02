e1576d6222f703df2d8e9b28a1ad665ecbc165ee
aeonincode <khashayar.sasannia@hotmail.com>
2021-02-02 18:06:41 +0100

    Connecting Mongoose to Mongo

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
 require('./routes/authRoutes')(app);
 
-// Your root index.js connect function should still look like this:
-// mongoose.connect(keys.mongoURI);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-// http://localhost:5000/
