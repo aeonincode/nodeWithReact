f711867eb87fe87be886cb3b0e8a058fa754a78d
aeonincode <khashayar.sasannia@hotmail.com>
2021-02-04 14:57:41 +0100

    Saving model instances

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,10 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+// User is model class
+const User = mongoose.model('User');
 
 passport.use(
   new GoogleStrategy(
@@ -9,10 +13,17 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
     },
+    // callback function
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
+      // inside this callback function use model class to create new instance of a user
+      // then pass object that contains all the different properties this user will have
+      // profile.id is the id coming from users google profile
+      // to get model instance to persistence self to the users collection or to persist it self to the MongoDB database we have to call a function on that method called .save(), so when we call save it will automatically take this record it will take that model instance and it will save it to the database
+      new User({ googleId: profile.id }).save();
+
+      // console.log('access token', accessToken);
+      // console.log('refresh token', refreshToken);
+      // console.log('profile', profile);
     }
   )
 );
