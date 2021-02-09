ec98df37111943e7825db48d77f08ff3d86167da
aeonincode <khashayar.sasannia@hotmail.com>
2021-02-09 17:16:11 +0100

    Enabling Cookies

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+// give access to cookies
+const cookieSession = require('cookie-session');
+// tell passport to make use of them
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -7,6 +11,17 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+// tell express that it needs to make use of of cookies inside our application
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+// tell passport use of cookies to handle authentication
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
