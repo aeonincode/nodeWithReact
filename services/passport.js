0527075bf05dfbfe95376e6886e931a6e90f4e67
aeonincode <khashayar.sasannia@hotmail.com>
2021-02-09 14:47:34 +0100

    Encoding Users

--- services/passport.js
+++ services/passport.js
@@ -6,6 +6,10 @@ const keys = require('../config/keys');
 // User is model class
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
 passport.use(
   new GoogleStrategy(
     {
