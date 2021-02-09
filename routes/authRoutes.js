ef146bf3db25319e5539582eae3d06bb23c574b2
aeonincode <khashayar.sasannia@hotmail.com>
2021-02-09 19:33:05 +0100

    Testing Authentication

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,10 @@ module.exports = (app) => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  // req represent incoming request, res represent outgoing response
+  app.get('/api/current_user', (req, res) => {
+    // test to make sure that someone who has already gone to OAuth flow and in theory logged into our application and get access to user
+    res.send(req.user);
+  });
 };
