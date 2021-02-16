d3bd5a358a85b62754d794973f24bb788fb1e7d7
aeonincode <khashayar.sasannia@hotmail.com>
2021-02-16 13:36:30 +0100

    modify .gitignore

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID:
+    '196433121017-ppmhfiol49ftk59kmfrjrjfvlnm6huuf.apps.googleusercontent.com',
+  googleClientSecret: 'PgmCFEbIAaoEF6mTsdXNUEn3',
+  mongoURI:
+    'mongodb+srv://todoAppUser:hala12ch@cluster0.anmcn.mongodb.net/emailydb?retryWrites=true&w=majority',
+  cookieKey: 'redacted',
+};
+
+// http://localhost:5000/
