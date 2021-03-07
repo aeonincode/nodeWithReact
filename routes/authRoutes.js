764ea2fa7a933650d42c6a792ad330af7c57eb4b
aeonincode <khashayar.sasannia@hotmail.com>
2021-03-07 01:50:33 +0100

    Redirecting a User on Auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,17 +8,20 @@ module.exports = (app) => {
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
 
   app.get('/api/logout', (req, res) => {
     req.logout();
-    // send back some acknowledgement to user saying you are no longer signed anymore
     res.send(req.user);
   });
 
-  // req represent incoming request, res represent outgoing response
   app.get('/api/current_user', (req, res) => {
-    // test to make sure that someone who has already gone to OAuth flow and in theory logged into our application and get access to user
     res.send(req.user);
   });
 };
